6541ed7474e7c01179fd2c4bc8e79907afcc2c24
rollwagen <rollwagen@gmail.com>
2021-03-02 19:22:48 +0000

    Updates...

--- aws/ubuntu_development_vm/ubuntu_dev_vm.tf
+++ aws/ubuntu_development_vm/ubuntu_dev_vm.tf
@@ -12,52 +12,58 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
 }
 
+# export TF_VAR_sg_inbound_ip=`curl 'https://api.ipify.org?format=text'`/32
+variable "sg_inbound_ip" {
+  type    = string
+  default = "0.0.0.0/0" 
+}
 
-resource "aws_vpc" "my-vpc" {
+
+resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "my-subnet" {
-  vpc_id     = aws_vpc.my-vpc.id
+  vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "eu-central-1a"
 }
 resource "aws_subnet" "my-subnet-b" {
-  vpc_id     = aws_vpc.my-vpc.id
+  vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "eu-central-1b"
 }
 resource "aws_subnet" "my-subnet-c" {
-  vpc_id     = aws_vpc.my-vpc.id
+  vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "eu-central-1c"
 }
 
 resource "aws_internet_gateway" "my-internet-gateway" {
-  vpc_id = aws_vpc.my-vpc.id
+  vpc_id = aws_vpc.dev-vpc.id
 }
 
 resource "aws_route" "my-route" {
-  route_table_id            = aws_vpc.my-vpc.main_route_table_id
+  route_table_id            = aws_vpc.dev-vpc.main_route_table_id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.my-internet-gateway.id
 }
 
 resource "aws_security_group_rule" "example" {
   type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["${var.sg_inbound_ip}"]
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  security_group_id = aws_vpc.my-vpc.default_security_group_id
+  security_group_id = aws_vpc.dev-vpc.default_security_group_id
 }
 resource "aws_security_group_rule" "security_rule_http" {
   type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = aws_vpc.my-vpc.default_security_group_id
+  cidr_blocks       = ["${var.sg_inbound_ip}"]
+  from_port         = 60000
+  to_port           = 61000
+  protocol          = "udp"
+  security_group_id = aws_vpc.dev-vpc.default_security_group_id
 }
 
 resource "aws_instance" "my-ec2-instance" {
@@ -74,8 +80,11 @@ resource "aws_instance" "my-ec2-instance" {
     
     apt update
     DEBIAN_FRONTEND=noninteractive apt upgrade -y
-    DEBIAN_FRONTEND=noninteractive apt install -y apache2
-    echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+    DEBIAN_FRONTEND=noninteractive apt install -y nmap mosh
+
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    DEBIAN_FRONTEND=noninteractive apt install -y terraform
 
     EOF
 }
