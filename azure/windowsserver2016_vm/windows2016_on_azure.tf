cb043a53f42b652e9edb29511885a7b42501873f
rollwagen <rollwagen@gmail.com>
2021-06-04 18:54:20 +0200

    Updated terrascan skips

--- azure/windowsserver2016_vm/windows2016_on_azure.tf
+++ azure/windowsserver2016_vm/windows2016_on_azure.tf
@@ -55,7 +55,6 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-#ts:skip=accurics.azure.NS.161 "Ensure subnet has NSG: seems false postitive b/c nsg get configure in tf file."
   name                = "vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -63,6 +62,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
+#ts:skip=accurics.azure.NS.161 "Ensure subnet has NSG: seems false postitive b/c nsg get configure in tf file."
   name                = "subnet"
   resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name= azurerm_virtual_network.vnet.name
