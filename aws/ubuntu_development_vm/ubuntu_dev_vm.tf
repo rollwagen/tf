1854991c7a1142e49e00dd19e912e13f9b98f07c
rollwagen <rollwagen@gmail.com>
2021-12-30 12:00:24 +0100

    Add terraform-fmt pre-commit and reformat

--- aws/ubuntu_development_vm/ubuntu_dev_vm.tf
+++ aws/ubuntu_development_vm/ubuntu_dev_vm.tf
@@ -26,18 +26,18 @@ resource "aws_vpc" "dev-vpc" {
 }
 
 resource "aws_subnet" "my-subnet" {
-  vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-central-1a"
 }
 resource "aws_subnet" "my-subnet-b" {
-  vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-central-1b"
 }
 resource "aws_subnet" "my-subnet-c" {
-  vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "eu-central-1c"
 }
 
@@ -46,9 +46,9 @@ resource "aws_internet_gateway" "my-internet-gateway" {
 }
 
 resource "aws_route" "my-route" {
-  route_table_id            = aws_vpc.dev-vpc.main_route_table_id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = aws_internet_gateway.my-internet-gateway.id
+  route_table_id         = aws_vpc.dev-vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my-internet-gateway.id
 }
 
 resource "aws_security_group_rule" "sg-rule-ssh-inbound" {
@@ -92,13 +92,13 @@ resource "aws_instance" "my-ec2-instance" {
   #ami           = "ami-0932440befd74cdba"
   #instance_type = "t2.micro"
   associate_public_ip_address = "true"
-  key_name = "id_rsa.pub"
+  key_name                    = "id_rsa.pub"
   #checkov:skip=CKV_AWS_88:This instance requires a public IP (direct SSH access)
   subnet_id = aws_subnet.my-subnet.id
   root_block_device { encrypted = "true" }
   monitoring = "true"
   metadata_options {
-    http_tokens = "required"
+    http_tokens   = "required"
     http_endpoint = "enabled"
   }
 
@@ -145,6 +145,6 @@ resource "aws_instance" "my-ec2-instance" {
 }
 
 output "public_ip" {
-  value       = aws_instance.my-ec2-instance.public_ip
+  value = aws_instance.my-ec2-instance.public_ip
 }
 
